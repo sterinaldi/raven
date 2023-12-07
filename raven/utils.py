95776e9d204575be40ecf1431f0a06951170c0e9
sterinaldi <stefano.rinaldi@unipd.it>
2023-12-07 11:06:36 +0100

    improved numerical integration

--- raven/utils.py
+++ raven/utils.py
@@ -1,5 +1,7 @@
 import numpy as np
 from pathlib import Path
+from numba import njit
+from tqdm import tqdm
 from scipy.stats import norm
 from scipy.interpolate import interp1d
 from figaro.utils import make_gaussian_mixture
@@ -10,7 +12,7 @@ class median_reconstruction:
     Wrapper to add the .pdf method to SciPy's interp1d
     """
     def __init__(self, x, p_x):
-        self.interpolant = interp1d(x, p_x, bounds_error = False, fill_value = 0.)
+        self.interpolant = interp1d(x, p_x, bounds_error = False, fill_value = 'extrapolate', kind = 'cubic')
     
     def pdf(self, x):
         return self.interpolant(x)
@@ -41,7 +43,7 @@ def make_mixtures(folder, rel_error = 0.03):
         if (data[:,1] == 0.).any():
             flag_no_errors = True
             idx = np.where(data[:,1] == 0)[0]
-            data[:,1][idx] = data[:,0][idx]*rel_error
+            data[:,1][idx] = np.abs(data[:,0][idx]*rel_error)
         if flag_no_errors == True:
             with open(Path(parent_folder, 'no_errors.txt'), 'a') as f:
                 f.write('{0}: star with no uncertainty. {1:.0f}% by hand.\n'.format(star_name, rel_error*100.))
@@ -67,14 +69,14 @@ def find_mean_weight(draws, vel_disp = 5.):
         double: relative weight of the specific feature in the mixture
     """
     bounds = draws[0].bounds
-    x      = np.linspace(bounds[0,0], bounds[0,1], 1000)
+    x      = np.linspace(bounds[0,0], bounds[0,1], 10000)
     prob   = np.median([d.pdf(x) for d in draws], axis = 0)
     prob   = prob/np.sum(prob*(x[1]-x[0]))
     mu     = x[np.argmax(prob)]
     max_p  = np.max(prob)
     return mu, max_p/norm(mu, vel_disp).pdf(mu)
 
-def probability_single_star(star, median, mu = None, weight = None, vel_disp = 5., n_draws = 1e4):
+def probability_single_star(star, median, mu, weight, bounds, vel_disp = 5., n_pts = 1e5):
     """
     Probability for an object to be a single star.
     
@@ -84,17 +86,15 @@ def probability_single_star(star, median, mu = None, weight = None, vel_disp = 5
         double mu: mean of the single star distribution. If None, is inferred from draws directly.
         double weight: relative weight of the single star component in the overall radial velocity distribution. If None, is inferred from draws directly.
         double vel_disp: velocity dispersion for single stars
-        double n_draws: number of MC draws
     
     Returns:
         double: probability for the object to be a single star
     """
-    # TODO: rewrite with analytical integral (now p slightly > 1 from time to time)
-    if mu is None or weight is None:
-        mu, weight = find_mean_weight(draws, vel_disp)
     # Integrals
     single_dist = norm(mu, vel_disp)
-    non_par     = MC_integral(median, star, n_draws = n_draws, error = False)
-    p_single    = MC_integral(single_dist, star, n_draws = n_draws, error = False)
-    p_binary    = (non_par - weight*p_single)/(1. + weight)
-    return p_single/(p_single + p_binary)
+    x           = np.linspace(bounds[0,0], bounds[0,1], int(n_pts))
+    dx          = x[1]-x[0]
+    non_par     = np.sum(median.pdf(x)*star.pdf(x)*dx)
+    p_single    = np.sum(single_dist.pdf(x)*star.pdf(x)*dx)
+    p_binary    = (non_par - weight*p_single)/(1. - weight)
+    return np.min([p_single/(p_single + p_binary), 1.])
